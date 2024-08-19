0a63116f0923616048125a84455b7228d6e623ca
Santiago Botta <sgobotta@gmail.com>
2024-08-19 20:46:48 -0300

    Layout update

--- src/App.tsx
+++ src/App.tsx
@@ -38,23 +38,26 @@ function App() {
       <header className="
         App-header absolute w-full z-40
       ">
-        <div className='
-          h-32
-          w-4/12 sm:w-2/12 md:w-1/6 xl:w-1/12 2xl:w-1/12
-          backdrop-filter backdrop-invert hover:backdrop-brightness-150 hover:backdrop-blur-3xl backdrop-blur-md
-          -translate-x-4 hover:-translate-x-0 scale-100 active:scale-95 active:-translate-x-6
-          fixed top-24 flex items-center
-          shadow-2xl
-          bg-gradient-to-r from-primary-500/0 via-accent-p-500/30 to-secondary-500/0
-          border-accent-s-900/70 border-y-[1px] border-r-[1px]
-          rounded-e-xl
-          transition duration-300
-          opacity-100 hover:bg-accent-p-300/0
-        '>
-          <div className='absolute right-2 z-50'>
-            <img src={livedj} className="App-logo" alt="livedj logo" />
+        <a href="https://dj.liveapps.com.ar">
+          <div className='
+            animate-[bounce_3s_infinite]
+            h-32
+            w-4/12 sm:w-2/12 md:w-1/6 xl:w-1/12 2xl:w-1/12
+            backdrop-filter backdrop-invert hover:backdrop-brightness-150 hover:backdrop-blur-3xl backdrop-blur-md
+            -translate-x-4 hover:-translate-x-0 scale-100 active:scale-95 active:-translate-x-6
+            fixed top-24 flex items-center
+            shadow-2xl
+            bg-gradient-to-r from-primary-500/0 via-accent-p-500/30 to-secondary-500/0
+            border-accent-s-900/70 border-y-[1px] border-r-[1px]
+            rounded-e-xl
+            transition duration-300
+            opacity-100 hover:bg-accent-p-300/0
+          '>
+            <div className='absolute right-2 z-50'>
+              <img src={livedj} className="App-logo" alt="livedj logo" />
+            </div>
           </div>
-        </div>
+        </a>
       </header>
       <div className="
         grid grid-rows-3 grid-cols-3 gap-0 grow
@@ -70,8 +73,10 @@ function App() {
             onDragStart={preventDefault}
             onDrop={preventDefault}
             className={`
-              border-[1px]] transition duration-[0.5s] active:-translate-x-0 active:translate-y-0 active:scale-x-[-1]
-              hover:rounded-2xl hover:scale-[1.2] ${index % 2 ? 'hover:-rotate-12' : 'hover:rotate-12'}
+              border-[1px]] transition duration-[0.75s] active:-translate-x-0 active:translate-y-0 active:scale-x-[-0.8] active:scale-y-[0.8]
+              hover:rounded-2xl hover:scale-[1.2] ${index % 2 ? 'hover:-rotate-6 active:rotate-0' : 'hover:rotate-6 active:-rotate-0'}
+              hover:scale-[1.75] hover:z-50 hover:!border-[0px]
+              grayscale hover:grayscale-0
             `}
             key={imageKey(index)}
           >
@@ -79,8 +84,8 @@ function App() {
               src={process.env.PUBLIC_URL + imagePath}
               alt={`image-${index}`}
               className='
-                transition-all duration-[3s]
-                hover:rounded-3xl
+                transition-all duration-[2s] ease-[cubic-bezier(0.035,0.795,0.05,0.95)]
+                hover:rounded-xl
                 hover:translate-x-3 hover:-translate-y-3
                 opacity-80 hover:opacity-100
                 border-[1px] border-accent-s-900/80
