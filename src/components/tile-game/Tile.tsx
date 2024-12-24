d2b0e5f51391bf4885629ccb319961ad23b28ed3
Santiago <sgobotta@gmail.com>
2024-12-23 22:52:11 -0300

    Improve on click tile animations

--- src/components/tile-game/Tile.tsx
+++ src/components/tile-game/Tile.tsx
@@ -1,3 +1,4 @@
+import { BaseSyntheticEvent, useEffect, useState } from 'react';
 import { useTile } from '../../hooks/tile-game/useTile';
 import { TileState } from '../../types';
 import { Tile } from '../../types';
@@ -14,54 +15,107 @@ export default function TileComponent({
   state,
 }: Tile): React.ReactElement {
   const tileAPI = useTile();
-  // border-[1px] hover:border-2 border-zinc-600 transition duration-[0.75s]
-  // active:scale-x-[-0.8] active:scale-y-[0.8]
 
-  // hover:rounded-t-md ${index % 2 ? 'hover:rounded-r-md active:rotate-0' : 'hover:rounded-l-md hover:rotate-6 active:-rotate-0'}
-  // ${index % 2 ? 'hover:-rotate-6 active:rotate-0' : 'hover:rotate-6 active:-rotate-0'}
-  // hover:scale-[1] hover:border-dashed hover:border-zinc-300
-  // grayscale
+  const [getAssetImage, setAssetImage] = useState<string>(asset.image);
 
   function renderImage(state: TileState, index: number, imagePath: string) {
     const _render = (
       imagePath: string,
       index: number,
-      extraClasses: string[] = [],
+      {
+        extraClasses = [],
+        flippable = false,
+      }: {
+        extraClasses: string[];
+        flippable: boolean;
+      },
     ): React.ReactElement => (
-      <img
-        src={buildImagePath(imagePath)}
-        alt={`image-${index}`}
-        className={`
-          transition-all duration-[2s] ease-[cubic-bezier(0.035,0.795,0.05,0.95)]
-          rounded-sm
-          opacity-80
-          border-[1px] border-accent-s-900/80 
-          ${extraClasses.join(', ')}
-        `}
-        fetchPriority={index < 16 ? 'high' : 'low'}
-      />
+      <div className="flip-card aspect-square">
+        <div
+          className={`
+            flex flex-col justify-center
+            flip-card-inner
+            relative transition duration-[0.8s]
+            ${flippable ? 'flippable' : 'idle'}
+          `}
+        >
+          <div className="flip-card-front">
+            <div>
+              <div className="flex justify-center">
+                <img
+                  src={buildImagePath('images/back-tile.png')}
+                  alt={`image-${index}`}
+                  className={`
+                    transition-all duration-[2s] ease-[cubic-bezier(0.035,0.795,0.05,0.95)]
+                    rounded-sm
+                    opacity-80
+                    border-[1px] border-accent-s-900/80 
+                    ${extraClasses.join(', ')}
+                    
+                  `}
+                  fetchPriority={index < 16 ? 'high' : 'low'}
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            className="
+            flip-card-back flex flex-col lign-center
+            absolute justify-evenly h-full w-full
+          "
+          >
+            <img
+              src={buildImagePath(imagePath)}
+              alt={`image-${index}`}
+              className={`
+                transition-all duration-[2s] ease-[cubic-bezier(0.035,0.795,0.05,0.95)]
+                rounded-sm
+                opacity-80
+                border-[1px] border-accent-s-900/80 
+                ${extraClasses.join(', ')}
+              `}
+              fetchPriority={index < 16 ? 'high' : 'low'}
+            />
+          </div>
+        </div>
+      </div>
     );
 
     switch (state) {
       case TileState.Hidden:
-        return _render('images/back-tile.png', index, [
-          // 'hover:translate-x-3 hover:-translate-y-3',
-          `${index % 2 ? 'hover:rounded-r-md sm:hover:-rotate-6 sm:active:rotate-0' : 'hover:rounded-l-md sm:hover:rotate-6 sm:active:-rotate-0'}`,
-          'hover:rounded-md hover:rounded-t-md hover:opacity-100',
-        ]);
+        const extraClasses = [
+          'hover:translate-x-3 hover:-translate-y-3',
+          `${index % 2 ? 'hover:sm:-rotate-6 hover:rounded-r-md sm:active:rotate-0' : 'hover:rounded-l-md'}`,
+          'hover:sm:rotate-6 hover:rounded-md hover:rounded-t-md hover:opacity-100',
+        ];
+        return _render(imagePath, index, { extraClasses, flippable: false });
 
       case TileState.Selected:
-        return _render(imagePath, index);
+        return _render(imagePath, index, { extraClasses: [], flippable: true });
 
       case TileState.Matched:
-        return _render(imagePath, index);
+        return _render(imagePath, index, { extraClasses: [], flippable: true });
     }
   }
 
   const _onClick =
     state === TileState.Hidden
-      ? (e: React.MouseEvent) => onClick!(e, id, tileAPI)
+      ? (e: BaseSyntheticEvent) => {
+          e.preventDefault();
+          onClick!(e, id, tileAPI);
+        }
       : () => {};
+
+  useEffect(() => {
+    if (state === TileState.Hidden) {
+      setTimeout(() => {
+        setAssetImage('images/back-tile.png');
+      }, 300);
+    } else {
+      setAssetImage(asset.image);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state]);
 
   return (
     <div
@@ -75,7 +129,7 @@ export default function TileComponent({
       `}
       key={id}
     >
-      {renderImage(state, index, asset.image)}
+      {renderImage(state, index, getAssetImage)}
     </div>
   );
 }
