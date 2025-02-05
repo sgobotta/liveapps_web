f9265fe4811d6993db45c2ad62f25a4b39896f70
Santiago <sgobotta@gmail.com>
2025-02-05 02:18:32 -0300

    WIP

--- src/components/app-selection/ApplicationSelection.tsx
+++ src/components/app-selection/ApplicationSelection.tsx
@@ -5,6 +5,58 @@ import ApplicationOptionComponent from './ApplicationOption';
 import { useMouseWheel } from '../../hooks/listeners/useScrolling';
 // import TileGameComponent from './components/tile-game/TileGame';
 
+function ApplicationPreview(): ReactElement {
+  return (
+    <div className='relative p-10 h-64 w-64'>
+      <div className='
+        absolute top-[50%] left-[40%]
+        bg-accent-p-300/20
+        h-60 w-60
+        rounded-3xl
+        blur-sm
+      '/>
+      <div className='
+        absolute top-[32%] left-[20%]
+        bg-accent-p-300/100
+        h-32 w-32
+        rounded-[3.0rem]
+        blur-[0.12rem]
+      '/>
+      <div className='
+        absolute top-[35%] left-[78%]
+        bg-accent-p-300/100
+        h-32 w-32
+        rounded-[3.0rem]
+        blur-[0.12rem]
+      '/>
+      <div className='
+        absolute top-[100%] left-[90%]
+        bg-rose-400/100
+        h-32 w-32
+        rounded-[3.0rem]
+        blur-[0.1rem]
+      '/>
+      <div className='
+        absolute top-[88%] left-[28%]
+        bg-accent-p-300/100
+        h-32 w-32
+        rounded-[3.0rem]
+        blur-[0.1rem]
+      '/>
+      <div className='
+        absolute top-[40%] left-[30%]
+        bg-accent-p-300/100
+        h-60 w-60
+        rounded-3xl
+        blur-[0.07rem]
+      '>
+        <p className='text-white'>
+        </p>
+      </div>
+    </div>
+  )
+}
+
 export default function ApplicationSelection(): ReactElement {
   const {
     chooseSelection,
@@ -72,24 +124,34 @@ export default function ApplicationSelection(): ReactElement {
   }
 
   return (
-    <div
-      className="
-        font-mono m-auto sm:w-full md:w-full lg:w-full xl:w-3/4 2xl:w-3/4
-        sm:p-24 md:p-24 lg:p-24 align-middle
-        absolute flex flex-col items-start
-        top-1/2 sm:left-1/2
-        transform sm:-translate-x-1/2 -translate-y-1/2
-        text-3xl
-        pl-10
-      "
-    >
-      {selections.map((selection: AppSelectionOption) => (
-        <ApplicationOptionComponent
-          isSelected={isSelected(currentSelection.choice, selection.choice)}
-          name={selection.name}
-          onClick={() => chooseSelection(selection)}
-        />
-      ))}
+    <div>
+      <div className='
+        w-full
+        align-middle
+        absolute flex flex-col items-center
+        top-[10%]
+      '>
+        <ApplicationPreview />
+      </div>
+      <div
+        className="
+          font-mono m-auto sm:w-full md:w-full lg:w-full xl:w-3/4 2xl:w-3/4
+          sm:p-24 md:p-24 lg:p-24 align-middle
+          absolute flex flex-col items-start
+          top-3/4 sm:top-3/4 sm:left-1/2
+          transform sm:-translate-x-1/2 -translate-y-1/2
+          text-3xl
+          pl-10
+        "
+      >
+        {selections.map((selection: AppSelectionOption) => (
+          <ApplicationOptionComponent
+            isSelected={isSelected(currentSelection.choice, selection.choice)}
+            name={selection.name}
+            onClick={() => chooseSelection(selection)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
