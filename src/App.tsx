4ad050226629088552347455e4a0dafb4c479d43
Santiago <sgobotta@gmail.com>
2025-02-11 18:51:18 -0300

    Remove old livedj header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import livedj from './svg/live-dj-iso-black.svg';
 import { ReactComponent as Octocat } from './svg/github.svg';
 import './App.css';
 import { ReactElement } from 'react';
@@ -7,34 +6,6 @@ import ApplicationSelection from './components/app-selection/ApplicationSelectio
 function App(): ReactElement {
   return (
     <div className="App h-screen max-h-screen overflow-y-hidden">
-      <header
-        className="
-        App-header absolute w-full z-40
-      "
-      >
-        <a href="https://dj.liveapps.com.ar" target="_blank" rel="noreferrer">
-          <div
-            className="
-              transition duration-500
-              animate-fade-right animate-infinite animate-duration-[5000ms] animate-ease-in-out animate-alternate animate-fill-forwards
-              h-20 w-20
-              hover:animate-pulse
-              backdrop-filter backdrop-invert hover:backdrop-brightness-150 hover:backdrop-blur-3xl backdrop-blur-md
-              -translate-x-4 hover:-translate-x-0 scale-100 active:scale-95 active:-translate-x-6
-              fixed top-24 flex items-center
-              shadow-2xl
-              bg-gradient-to-r from-primary-500/0 via-accent-p-500/30 to-secondary-500/0
-              border-accent-s-900/70 border-y-[1px] border-r-[1px]
-              rounded-e-xl
-              opacity-100 hover:bg-accent-p-300/0
-            "
-          >
-            <div className="absolute right-2 z-50">
-              <img src={livedj} className="App-logo" alt="livedj logo" />
-            </div>
-          </div>
-        </a>
-      </header>
       <ApplicationSelection />
       <div className="fixed bottom-5 right-5 z-50 grayscale hover:grayscale-0 duration-500 translate-x-0 hover:translate-x-0 translate-y-52 hover:translate-y-0">
         <iframe
