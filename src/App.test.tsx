39df81776c4728eee152adb380ce64efd03f7e07
Santiago <sgobotta@gmail.com>
2025-02-13 01:30:20 -0300

    Remove test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  // render(<App />);
   // const linkElement = screen.getByText(/liveapps/i);
   // expect(linkElement).toBeInTheDocument();
 });
