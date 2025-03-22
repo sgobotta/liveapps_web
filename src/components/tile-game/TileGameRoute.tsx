dc354a9c73aa46c83bfed5ad9a6582437618d89e
Santiago <sgobotta@gmail.com>
2025-03-22 01:45:34 -0300

    Refactor the modal window to an Alert component

--- src/components/tile-game/TileGameRoute.tsx
+++ src/components/tile-game/TileGameRoute.tsx
@@ -6,6 +6,7 @@ import { TileAsset as TileAssetT } from '../../types';
 import TileGame from './TileGame';
 import { HomeIcon } from '../../svg/HomeIcon';
 import { useNavigate } from 'react-router';
+import Alert from '../ui/Alert';
 
 export default function TileGameRoute(): ReactElement {
   const navigate = useNavigate();
@@ -17,10 +18,12 @@ export default function TileGameRoute(): ReactElement {
     (image: string): TileAssetT => createTileAsset(image),
   );
 
-  function startGame() {}
+  function startGame() {
+    
+  }
 
   return (
-    <div>
+    <div className='select-none'>
       <div
         className="
           absolute top-3 left-3
@@ -32,40 +35,12 @@ export default function TileGameRoute(): ReactElement {
       >
         <HomeIcon />
       </div>
-      <div className="absolute w-full h-full z-[9]">
-        <div className="flex flex-row justify-center h-full items-center">
-          <div
-            className="
-            w-full h-[200px] mx-8 sm:mx-0
-            sm:w-[400px]
-            text-zinc-300 
-            bg-gradient-to-br from-zinc-800 via-zinc-700 to-zinc-800 from-30% to-90%
-            border-[1px] border-zinc-800/30
-            flex flex-col
-            gap-8 p-6 justify-items-center
-            shadow-2xl
-            rounded-[6px]
-            items-center
-            justify-center
-          "
-          >
-            <p className="text-xl font-normal">
-              Press start to shuffle pictures!
-            </p>
-            <button
-              onClick={startGame}
-              className="
-                w-fit
-                py-2 px-6 rounded-lg shadow-lg
-                bg-zinc-200 border-zinc-900/30 border-[1px] text-zinc-900
-                pressable font-bold hover:underline active:italic transition-all duration-500
-              "
-            >
-              Start
-            </button>
-          </div>
-        </div>
-      </div>
+      <Alert
+        onConfirm={startGame}
+        confirmText='Start'
+        contentText='Press start to shuffle pictures!'
+        visible={false}
+      />
       <div className="blur-sm h-screen">
         <TileGame tiles={tileAssets} />
       </div>
